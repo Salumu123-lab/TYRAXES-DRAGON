254613faa755b9a525ed9eb6b262af568bab24cb
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 20:16:33 +0300

    TYRAXES 📢

--- plugins/gc-warns.js
+++ plugins/gc-warns.js
@@ -1,13 +1,13 @@
 let handler = async(m, { conn, args, groupMetadata}) => {
         let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-        if (!(who in global.db.data.users)) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] The user is not found in my database*`
+        if (!(who in global.db.data.users)) throw `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] The user is not found in my database*`
         let warn = global.db.data.users[who].warn
         let name = conn.getName(who)
        m.reply(`
   *WARNINGS*
 
-🐝 *Name :* ${name}
-🐝 *Warns :* ${warn}`)
+🇰🇪 *Name :* ${name}
+🇰🇪 *Warns :* ${warn}`)
 }
 
 handler.help = ['warns']
