d25d87a0c04428547249f42dc210001d4f823a63
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 15:20:10 +0300

    TYRAXES 📢

--- plugins/tools-getbio.js
+++ plugins/tools-getbio.js
@@ -6,13 +6,13 @@ let handler = async (m, { conn, text, command }) => {
     let bio = await conn.fetchStatus(who)
     m.reply(bio.status)
   } catch {
-    if (text) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] bio is private!*`
+    if (text) throw `ARLODRAGON IS MY OWNER*[❗𝐈𝐍𝐅𝐎❗] bio is private!*`
     else try {
       let who = m.quoted ? m.quoted.sender : m.sender
       let bio = await conn.fetchStatus(who)
       m.reply(bio.status)
     } catch {
-      throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] bio is private!*`
+      throw `ARLODRAGON IS MY OWNER*[❗𝐈𝐍𝐅𝐎❗] bio is private!*`
     }
   }
 }
