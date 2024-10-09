7fc160d8fa1d467c0f7b29a329327413160a855d
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 17:24:25 +0300

    TYRAXES 📢

--- plugins/owner-getsessi.js
+++ plugins/owner-getsessi.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-    m.reply('https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗ ] Please wait, Downloading the bot session file*')
-    let sesi = await fs.readFileSync('./BumbleSession/creds.json')
+    m.reply('https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗ ] Please wait, Downloading the bot session file*')
+    let sesi = await fs.readFileSync('./Tyraxessession/creds.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'creds.json' }, { quoted: m })
 }
 handler.help = ['getsessi']
