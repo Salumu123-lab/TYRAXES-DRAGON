c1836f3a586f85ca52572ba7a2ebd1d921d45237
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 19:56:47 +0300

    TYRAXES 📢

--- plugins/gc-online.js
+++ plugins/gc-online.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, args, usedPrefix }) => {
       .map(({ jid, name }) => ({ jid, name }));
 
     if (filteredParticipants.length === 0) {
-        return conn.reply(m.chat, 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] No users online at the moment*', m);
+        return conn.reply(m.chat, 'https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] No users online at the moment*', m);
     }
 
     const formattedText = filteredParticipants.map(({ jid, name }, index) => (
