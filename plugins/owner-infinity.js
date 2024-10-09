e4211c442aeb4d9a9243aa600f09846598efb930
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 17:21:37 +0300

    TYRAXES 📢

--- plugins/owner-infinity.js
+++ plugins/owner-infinity.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗]  @${m.sender.split('@')[0]} now your resources are unlimited.*`, mentions: [m.sender]}, {quoted: m});
+        conn.sendMessage(m.chat, {text: `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗]  @${m.sender.split('@')[0]} now your resources are unlimited.*`, mentions: [m.sender]}, {quoted: m});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].limit = Infinity;
   global.db.data.users[m.sender].level = Infinity;
