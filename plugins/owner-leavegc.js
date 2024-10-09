98fb946c10898ff9c54b9fdd49fd0ee41260f83c
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 17:09:17 +0300

    TYRAXES

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Goodbye everyone, the Bot says goodbye! (≧ω≦)ゞ*')
+await conn.reply(id, 'https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] Goodbye everyone, The group is so weird😞! (≧ω≦)ゞ*')
 await conn.groupLeave(id)}
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
