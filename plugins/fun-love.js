358bd9c27f29e68ed47c20eeb75490e912afdb6a
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 19:11:34 +0300

    TYRAXES 📢

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `https://github.com/Khalid-official *❤️❤️ LOVE METER ❤️❤️*
+let love = `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *❤️❤️ LOVE METER ❤️❤️*
 *[❗𝐈𝐍𝐅𝐎❗] The love for ${text} For you it is* *${Math.floor(Math.random() * 100)}%* *of a 100%*
 *[❗𝐈𝐍𝐅𝐎❗] You should ask her to be your girlfriend*
 `.trim()
