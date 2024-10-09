085269df7b1f8cc3756676e7efe2c9d7f3a21ae1
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 21:52:41 +0300

    TYRAXES 📢

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -4,15 +4,15 @@ let handler = async(m, { conn, usedPrefix }) => {
      let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
      let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
-     m.reply(`https://github.com/Khalid-official 
+     m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q 
      [❗𝐈𝐍𝐅𝐎❗] 
 ≡ *USERS BANNED*
 
-🐝 Total : *${users.length}*
+📢 Total : *${users.length}*
 
 ${users ? '\n' + users.map(([jid], i) => `
 ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
-🐝 ${jid}
+📢 ${jid}
 `.trim()).join('\n') : ''}
 `.trim())
 }
