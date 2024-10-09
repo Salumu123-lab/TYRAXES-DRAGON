84e8a2b844955e20480eb4d6e989d36829847893
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 16:08:28 +0300

    TYRAXES 📢

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -2,12 +2,12 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-   if (!args[0]) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Enter serial number*\n*Verify your serial number with the command...*\n\n*${usedPrefix}nserial*`
+   if (!args[0]) throw `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] Enter serial number*\n*Verify your serial number with the command...*\n\n*${usedPrefix}nserial*`
    let user = global.db.data.users[m.sender]
    let sn = createHash('md5').update(m.sender).digest('hex')
    if (args[0] !== sn) throw '⚠️ *Incorrect serial number*'
    user.registered = false
-   m.reply(`https://github.com/Khalid-official *👌Record deleted*`)
+   m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *👌Record deleted*`)
 }
 handler.help = ['unreg <Serial Number>']
 handler.tags = ['rg']
