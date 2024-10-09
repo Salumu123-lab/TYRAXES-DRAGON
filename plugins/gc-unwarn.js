4c75e7e2bad827b6b10530e27cb61b68220e3d8c
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 20:15:34 +0300

    TYRAXES 📢

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -2,18 +2,18 @@ let handler = async(m, { conn, args, groupMetadata}) => {
          let who
          if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
          else who = m.chat
-         if (!who) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Tag or mention someone*`
-         if (!(who in global.db.data.users)) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] The user is not found in my database*`
+         if (!who) throw `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] Tag or mention someone*`
+         if (!(who in global.db.data.users)) throw `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] The user is not found in my database*`
         let warn = global.db.data.users[who].warn
         if (warn > 0) {
           global.db.data.users[who].warn -= 1
-          m.reply(`https://github.com/Khalid-official [❗𝐈𝐍𝐅𝐎❗] *DELWARN*
+          m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q [❗𝐈𝐍𝐅𝐎❗] *DELWARN*
          
 🐝 Warnings: *-1*
 🐝 Total Warns: *${warn - 1}*`)
           m.reply(`*[❗𝐈𝐍𝐅𝐎❗] An admin lowered his warning, now you have* *${warn - 1}*`, who)
           } else if (warn == 0) {
-             m.reply('https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] The user has no warning*')
+             m.reply('https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] The user has no warning*')
          }
 
 }
