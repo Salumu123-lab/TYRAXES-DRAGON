f69dc51c66fecbf10a75e12037793e76de9ec46a
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 20:25:33 +0300

    TYRAXES 📢

--- plugins/info-database.js
+++ plugins/info-database.js
@@ -1,7 +1,7 @@
 const handler = async (m) => {
   const totalreg = Object.keys(global.db.data.users).length;
   const rtotalreg = Object.values(global.db.data.users).filter((user) => user.msgs == true).length;
-  m.reply(`https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗]The current number of database  🗃️ users is  ${totalreg} users*`);
+  m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗]The current number of database  🗃️ users is  ${totalreg} users*`);
 };
 handler.help = ['database', 'user'];
 handler.tags = ['info'];
