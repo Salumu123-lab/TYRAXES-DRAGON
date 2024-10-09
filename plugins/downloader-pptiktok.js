f2a0839beac873387e22bb79fd4dca7b485c1811
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 18:50:57 +0300

    TYRAXES 📢

--- plugins/downloader-pptiktok.js
+++ plugins/downloader-pptiktok.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, text }) => {
-if (!text) throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] enter the name or the username of the tiktok*'
+if (!text) throw 'https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] enter the name or the username of the tiktok*'
 let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`
 conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ]  here is the profile photo of${text}*`, m, false)}
 handler.help = ['tiktokfoto'].map(v => v + ' <username>')
