73c4a3e852c4db35446f70b640596803ce0f44e2
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 18:46:54 +0300

    TYRAXES 📢

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,7 +1,7 @@
 import { pinterest } from '@bochilteam/scraper'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `https://github.com/Khalid-official ✳️ *[❗𝐈𝐍𝐅𝐎❗] What image do you want me to look for?*\n\n📌 Example  : ${usedPrefix + command} Lil Peep`
+  if (!text) throw `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q ✳️ *[❗𝐈𝐍𝐅𝐎❗] What image do you want me to look for?*\n\n📌 Example  : ${usedPrefix + command} Lil Peep`
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
 *▢  Pinterest:*  ${text}
