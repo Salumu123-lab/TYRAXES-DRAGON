7a90aea46c220abe08211f825c030b2129fc890d
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 21:37:04 +0300

    TYRAXES 📢

--- plugins/mipilot-stopbot.js
+++ plugins/mipilot-stopbot.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 async function handler(m, {conn, usedPrefix}) {
-   if (conn.user.jid == global.conn.user.jid) return m.reply(`https://github.com/Khalid-official *[❗] *You can't turn off the main Bot.*`)
-   m.reply(`https://github.com/Khalid-official *[❗] *You will stop being a Sub Bot in 5 seconds...*`)
+   if (conn.user.jid == global.conn.user.jid) return m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗] *You can't turn off the main Bot.*`)
+   m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗] *You will stop being a Sub Bot in 5 seconds...*`)
    conn.fstop = true
    conn.ws.close()
 }
