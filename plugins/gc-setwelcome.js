11d37a45803f7b6f835938e6089e8b8b3d168fee
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 20:11:01 +0300

    TYRAXES 📢

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text
-m.reply('https://github.com/Khalid-official *[❗] The welcome message is configured*')
-} else throw `https://github.com/Khalid-official *[❗] ENTER THE WELCOME MESSAGE YOU WANT TO ADD, 𝚄𝚂𝙴:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`
+m.reply('https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗] The welcome message is configured*')
+} else throw `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗] ENTER THE WELCOME MESSAGE YOU WANT TO ADD, 𝚄𝚂𝙴:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
