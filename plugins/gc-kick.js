8f0ba4093d52152bc136f12f89feb572ca4b1268
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 19:51:38 +0300

    TYRAXES 📢

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Correct use of command*\n*${usedPrefix + command}* @tag`
+let kickte = `https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] Correct use of command*\n*${usedPrefix + command}* @tag`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)})
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] user successfully removed*`)
+m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] user successfully removed*`)
 
 }
 
