82ef2fa49aef04c27a9f3cb24c515d16a74cbb41
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 16:52:29 +0300

    TYRAXES 📢

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -4,7 +4,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
     if (!m.quoted.text) throw `balas pesan nya!`
     let path = `${text}`
     await fs.writeFileSync(path, m.quoted.text)
-    m.reply(`https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗]  stored in* ${path}`)
+    m.reply(`https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗]  stored in* ${path}`)
 }
 handler.help = ['sf'].map(v => v + ' <teks>')
 handler.tags = ['owner']
