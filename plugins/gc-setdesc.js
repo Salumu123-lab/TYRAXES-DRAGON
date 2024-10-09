056e7e96ff83cce641d9d71c9d8844271ef45bf8
SIR-DRAGON <arlodeadpool@gmail.com>
2024-10-09 20:02:10 +0300

    TYRAXES 📢

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
     await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
-    m.reply('https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Success changing The description of the group*')
+    m.reply('https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q *[❗𝐈𝐍𝐅𝐎❗] Success changing The description of the group*')
     }
     handler.help = ['Setdesc <text>']
     handler.tags = ['group']
